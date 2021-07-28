8f0a65236e916319483126c2f5bb0e7b684469d7
sho <69189153+spadr@users.noreply.github.com>
2021-07-28 14:27:55 +0900

    Update __init__.py

--- app/iot/views/__init__.py
+++ app/iot/views/__init__.py
@@ -1,11 +1,5 @@
 from .make_user import signupfunc, completefunc
 from .sign_user import loginfunc, logoutfunc
-<<<<<<< HEAD
 from .create_data import DataReceiveApi, DataSendApi, DeviceSetApi, browserpostfunc
 from .view_page import readfunc, graphfunc, memefunc
 from .manipulate_data import consolefunc, profilefunc
-=======
-from .create_data import DataReceiveApi, DataSendApi, browserpostfunc
-from .view_page import readfunc, graphfunc, memefunc
-from .manipulate_data import consolefunc
->>>>>>> def25fd97447de12832996abc177a3138cb443c3
